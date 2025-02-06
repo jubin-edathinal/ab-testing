a21f6241c469b58fb19c9d76385b639aa5b4a6dd
jubin-edathinal <65755230+jubin-edathinal@users.noreply.github.com>
2025-02-06 15:47:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GXEWQPEM9F">
 
 // Function to randomly assign users to a variation
 function assignVariation() {
@@ -37,4 +36,4 @@ document.getElementById('button').addEventListener('click', function () {
     });
     console.log('Button clicked on Variation:', variation);
 });
-</script>
+
